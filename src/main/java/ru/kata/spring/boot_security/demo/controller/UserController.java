218284c15379b9c377d638333adc9c480ece587e
volRender <litvishko.render@gmail.com>
2023-04-19 13:28:36 +0300

    Starting add files from Boot project

---
+++ src/main/java/ru/kata/spring/boot_security/demo/controller/UserController.java
@@ -0,0 +1,57 @@
+package ru.kata.spring.boot_security.demo.controller;
+
+import com.litvishko.kata.springBootKataPP.model.User;
+import com.litvishko.kata.springBootKataPP.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequestMapping("/users")
+public class UserController {
+
+	private final UserService userService;
+
+	@Autowired
+	public UserController(UserService userService) {
+		this.userService = userService;
+	}
+
+	@GetMapping
+	public String printUsers(Model model) {
+		model.addAttribute("users", userService.allUsers());
+		return "users";
+	}
+
+	@GetMapping("/new")
+	public String newUser(Model model) {
+		model.addAttribute("newUser", new User());
+		return "newUser";
+	}
+
+	@PostMapping
+	public String saveUser(@ModelAttribute("newUser") User user) {
+		userService.addOrUpdateUser(user);
+		return "redirect:/users";
+	}
+
+	@GetMapping("/{id}/edit")
+	public String getUser(@PathVariable("id") long id, Model model) {
+		model.addAttribute("editedUser", userService.getUser(id));
+		return "editUser";
+	}
+
+	@PatchMapping("/{id}")
+	public String editUser(@ModelAttribute("editedUser") User user, @PathVariable("id") long id) {
+		user.setId(id);
+		userService.addOrUpdateUser(user);
+		return "redirect:/users";
+	}
+
+	@DeleteMapping("/{id}")
+	public String deleteUser(@PathVariable("id") long id) {
+		userService.deleteUser(id);
+		return "redirect:/users";
+	}
+}
